1bd46a88b041dae659ca9d2ff291f07adf835941
Rafael C. Ferreira <carvalhoferreirarafael@gmail.com>
2024-10-12 10:52:52 -0300

    [TOOLS] gauss: comentado prints de debug.

--- tools/gauss.py
+++ tools/gauss.py
@@ -8,38 +8,38 @@ def escalonamento(ma,mb):
 
     #Considera que a matrix é quadrada, verificar se é possível matrix não quadrada
     for j in range(colunas-1): #Itera entre as colunas menos a final
-        print(f"coluna {j}: {u[j:,j]}")
+        #print(f"coluna {j}: {u[j:,j]}")
         id_pivo = np.argmax(u[j:,j])+j
-        print(f"id_pivo coluna {j}: {id_pivo}")
+        #print(f"id_pivo coluna {j}: {id_pivo}")
         if id_pivo == j:
             pivo = u[j,id_pivo]
-            print(f"Pivo correto: {pivo}")
-            print(u)
+            #print(f"Pivo correto: {pivo}")
+            #print(u)
 
 
         else:
-            print(f"Trocando linhas {j} e {id_pivo} ")
-            print('U antigo')
-            print(u)
+            #print(f"Trocando linhas {j} e {id_pivo} ")
+            #print('U antigo')
+            #print(u)
             u[[j,id_pivo]] = u[[id_pivo,j]]
-            print('u novo')
-            print(u)
+            #print('u novo')
+            #print(u)
             n_trocas += 1
-            print(f" Troca nº: {n_trocas}")
+            #print(f" Troca nº: {n_trocas}")
             pivo = u[j,j]
-            print(f"Pivo: {pivo}")
+            #print(f"Pivo: {pivo}")
         for i in range(j+1,ordem): #Não itera a 1º linha
             m = u[i,j]/pivo
-            print(f"m = {u[i,j]}/{pivo} = {m}")
+            #print(f"m = {u[i,j]}/{pivo} = {m}")
             u[i,:] = u[i,:] - m*u[j,:]
-            print(f"L{i+1} = L{i+1} - {m}*L{j+1}")
-            print(u)
+            #print(f"L{i+1} = L{i+1} - {m}*L{j+1}")
+            #print(u)
         
-        print("-----------------------------------------")
+        #print("-----------------------------------------")
 
-    print(f"Ordem: {ordem}")
-    print(f"Nº trocas: {n_trocas}")
-    print(u)
+    #print(f"Ordem: {ordem}")
+    #print(f"Nº trocas: {n_trocas}")
+    #print(u)
     return u,ordem,n_trocas
 
 def res_sistema(u):
@@ -47,32 +47,32 @@ def res_sistema(u):
     res = []
 
     for n,i in enumerate(u[::-1]):
-        print(f"X{len(u)-n}")
-        print()
-        print(f"linha: {i}")
-        print(f"valor igualdade: {round(i[-1],3)}")
-        print(f"coeficientes: {i[-(n+1):-1]}")
+        #print(f"X{len(u)-n}")
+        #print()
+        #print(f"linha: {i}")
+        #print(f"valor igualdade: {round(i[-1],3)}")
+        #print(f"coeficientes: {i[-(n+1):-1]}")
 
         subtracao = 0
         for k,a in enumerate(i[-(n+1):-1]):
-            print(f"c inicial: {subtracao}")
-            print(f"res: {res}")
-            print(f"calculo: {a} * {res[k-1]}")
+            #print(f"c inicial: {subtracao}")
+            #print(f"res: {res}")
+            #print(f"calculo: {a} * {res[k-1]}")
             subtracao += a*res[k-1]
-            print(f"c final: {subtracao}")
-            print()
+            #print(f"c final: {subtracao}")
+            #print()
 
         x = (i[-1]-subtracao)/i[-(n+2)]
-        print()
-        print(f"X{len(u)-n} = {round(i[-1],3)} - {round(subtracao,3)} / {round(i[-(n+2)],3)}")
+        #print()
+        #print(f"X{len(u)-n} = {round(i[-1],3)} - {round(subtracao,3)} / {round(i[-(n+2)],3)}")
         subtracao = 0
         res.append(round(x,3))
-        print(f"X{len(u)-n} = {round(x,3)}")
-        print("----------------------------")
-        print()
+        #print(f"X{len(u)-n} = {round(x,3)}")
+        #print("----------------------------")
+        #print()
 
     res = res[::-1]
-    print(f"Resolução: {res}")
+    #print(f"Resolução: {res}")
     return res   
 
 def met_gauss(ma,mb):
